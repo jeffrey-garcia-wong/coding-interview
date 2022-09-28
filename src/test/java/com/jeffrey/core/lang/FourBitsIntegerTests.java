a81694f280a95438e680f3200fc30393d18ed714
jeffrey garcia <jeffrey-garcia@outlook.com>
2022-09-28 09:07:41 +0100

    enable parallel test execution

--- src/test/java/com/jeffrey/core/lang/FourBitsIntegerTests.java
+++ src/test/java/com/jeffrey/core/lang/FourBitsIntegerTests.java
@@ -1,12 +1,9 @@
 package com.jeffrey.core.lang;
 
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
 public class FourBitsIntegerTests {
 
     @Test
