ca42e5b586ebbb99ca462dd19abb6cdac76bb6b9
jeffrey garcia <jeffrey-garcia@outlook.com>
2022-10-12 11:30:16 +0100

    add MinimumLengthSubstring problem

---
+++ src/test/java/com/jeffrey/facebook/MinimumLengthSubstringsTests.java
@@ -0,0 +1,85 @@
+package com.jeffrey.facebook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MinimumLengthSubstringsTests {
+
+    @Test
+    public void test_001() {
+        String s = "dcbefebce";
+        String t = "fd";
+        assertEquals(5, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_002() {
+        String s = "dcbefebce";
+        String t = "fe";
+        assertEquals(2, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_003() {
+        String s = "dcbefebce";
+        String t = "fb";
+        assertEquals(3, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_004() {
+        String s = "dcbefebce";
+        String t = "af";
+        assertEquals(-1, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_005() {
+        String s = "dcbefebce";
+        String t = "ag";
+        assertEquals(-1, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test006() {
+        String s = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddfxyz";
+        String t = "xyz";
+        assertEquals(3, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_007() {
+        String s = "xbfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddfxyz";
+        String t = "xyz";
+        assertEquals(3, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_008() {
+        String s = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddf";
+        String t = "cbccfafebccdccebdd";
+        assertEquals(-1, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_009() {
+        String s = "efyhul";
+        String t = "y";
+        assertEquals(1, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_010() {
+        String s = "a";
+        String t = "a";
+        assertEquals(1, MinimumLengthSubstrings.execute(s, t));
+    }
+
+    @Test
+    public void test_011() {
+        String s = "";
+        String t = "";
+        assertEquals(-1, MinimumLengthSubstrings.execute(s, t));
+    }
+}
