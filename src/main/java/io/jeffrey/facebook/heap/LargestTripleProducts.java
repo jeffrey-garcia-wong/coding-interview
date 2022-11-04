223359ee5c097735e27b13d649bba7fe80853a44
jeffrey garcia <jeffrey-garcia@outlook.com>
2022-11-04 22:28:47 +0000

    add facebook problem largest triple products

--- src/main/java/io/jeffrey/facebook/heap/LargestTripleProducts.java
+++ src/main/java/io/jeffrey/facebook/heap/LargestTripleProducts.java
@@ -3,6 +3,53 @@ package io.jeffrey.facebook.heap;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/**
+ * <h1>Largest Triple Products</h1><p/>
+ *
+ * You're given a list of n integers arr[0..(n-1)]. You must compute a list
+ * output[0..(n-1)] such that, for each index i (between 0 and n-1, inclusive),
+ * output[i] is equal to the product of the three largest elements out of
+ * arr[0..i] (or equal to -1 if i < 2, as arr[0..i] then includes fewer than
+ * three elements).<br/>
+ *
+ * Note that the three largest elements used to form any product may have the
+ * same values as one another, but they must be at different indices in arr.<br/>
+ *
+ * <b>Input</b><br/>
+ * <pre>
+ * {@code
+ * n is in the range [1, 100,000].
+ * Each value arr[i] is in the range [1, 1,000].
+ * }
+ * </pre>
+ *
+ * <b>Output</b><br/>
+ * <pre>
+ * {@code
+ * Return a list of n integers output[0..(n-1)], as described above.
+ * }
+ * </pre>
+ *
+ * <b>Example 1</b><br/>
+ * <pre>
+ * {@code
+ * n = 5
+ * arr = [1, 2, 3, 4, 5]
+ * output = [-1, -1, 6, 24, 60]
+ * The 3rd element of output is 3*2*1 = 6, the 4th is 4*3*2 = 24, and the 5th is 5*4*3 = 60.
+ * }
+ * </pre>
+ *
+ * <b>Example 2</b><br/>
+ * <pre>
+ * {@code
+ * n = 5
+ * arr = [2, 1, 2, 1, 2]
+ * output = [-1, -1, 4, 4, 8]
+ * The 3rd element of output is 2*2*1 = 4, the 4th is 2*2*1 = 4, and the 5th is 2*2*2 = 8.
+ * }
+ * </pre>
+ */
 class LargestTripleProducts {
 
     static int[] execute(int[] arr) {
