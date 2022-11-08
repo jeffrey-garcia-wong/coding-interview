61ce98f36d8901170b3b9a54afc879c1c31528bd
jeffrey garcia <jeffrey-garcia@outlook.com>
2022-11-08 15:53:10 +0000

    revise all maths problem

---
+++ src/test/java/io/jeffrey/core/maths/GreatestCommonDivisiorTests.java
@@ -0,0 +1,66 @@
+package io.jeffrey.core.maths;
+
+import org.junit.jupiter.api.Test;
+
+import static io.jeffrey.core.maths.GreatestCommonDivisior.execute;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreatestCommonDivisiorTests {
+
+    @Test
+    public void test1() {
+        int a = 0;
+        int b = 21;
+        assertEquals(b, execute(a,b));
+    }
+
+    @Test
+    public void test2() {
+        int a = 17;
+        int b = 0;
+        assertEquals(a, execute(a,b));
+    }
+
+    @Test
+    public void test3() {
+        int a = 0;
+        int b = 0;
+        assertEquals(a, execute(a,b));
+    }
+
+    @Test
+    public void test4() {
+        int a = 27;
+        int b = 6;
+        assertEquals(3, execute(a,b));
+    }
+
+    @Test
+    public void test5() {
+        int a = 99;
+        int b = 55;
+        assertEquals(11, execute(a,b));
+    }
+
+    @Test
+    public void test6() {
+        int a = 5;
+        int b = 7;
+        assertEquals(1, execute(a,b));
+    }
+
+    @Test
+    public void test7() {
+        int a = 4;
+        int b = 6;
+        assertEquals(2, execute(a,b));
+    }
+
+    @Test
+    public void test8() {
+        int a = 11;
+        int b = 17;
+        assertEquals(1, execute(a,b));
+    }
+
+}
