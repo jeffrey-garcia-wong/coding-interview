432c0ee559b500c7a9a278e743cbc4e32d921fe0
jeffrey garcia <jeffrey-garcia@outlook.com>
2022-11-14 00:05:04 +0000

    add quicksort

---
+++ src/main/java/io/jeffrey/core/sortings/Quicksort.java
@@ -0,0 +1,43 @@
+package io.jeffrey.core.sortings;
+
+class Quicksort {
+
+    static void execute(int [] input) {
+        solutionV1(input, 0, input.length-1);
+    }
+
+    private static void solutionV1(int[] input, int start, int end) {
+        // base case of recursion, stop when the sub-array has
+        // only 1 element and we have nothing to sort
+        if (start >= end) return;
+
+        // pick the mid as pivot
+        int pivotVal = input[((end - start) >> 1) + start];
+
+        // move everything smaller than pivot to left, and
+        // everything bigger than pivot to right
+        // return the position of split
+        int splitIndex = partition(input, pivotVal, start, end);
+
+        // split the input array into 2 sub-arrays and continue sorting
+        solutionV1(input, start, splitIndex-1);
+        solutionV1(input, splitIndex, end);
+    }
+
+    private static int partition(int[] input, int pivotVal, int start, int end) {
+        while (start <= end) {
+            while (input[start] < pivotVal) start++;
+            while (input[end] > pivotVal) end--;
+
+            if (start <= end) {
+                int tmp = input[start];
+                input[start] = input[end];
+                input[end] = tmp;
+                start ++;
+                end --;
+            }
+        }
+
+        return start;
+    }
+}
