c8a73982a52a9e0835c6ff163ecd97ded8131ebb
jeffrey garcia <jeffrey-garcia@outlook.com>
2023-01-17 05:08:10 +0000

    revise test case

--- src/main/java/io/jeffrey/core/sortings/InsertionSort.java
+++ src/main/java/io/jeffrey/core/sortings/InsertionSort.java
@@ -7,16 +7,30 @@ public class InsertionSort {
     }
 
     private static int[] solutionV1(int[] input) {
-        int sortedSize = 0;
+        /*
+         * Design the algorithm here
+         * 1) iterate through all the elements in the outer loop,
+         *    and for each element at position i
+         * 2) iterate through all the elements before position i to 0
+         *    in the inner loop
+         * 3) compare each element at j with element at j+1
+         * 4) if the element at j+1 is smaller, swap it with the element at j
+         * 5) otherwise, break the inner loop and continue the next iteration
+         *    in step 1
+         * 6) the process is exhausted when the iteration of the outer loop
+         *    finish
+         */
+
         // Write the code here
         for (int i=0; i<input.length; i++) {
-            int tmp = input[i];
-            int j = i - 1;
-            while (j>=0 && tmp < input[j]) {
-                input[j+1] = input[j];
-                j -= 1;
+            for (int j=i-1; j>=0; j--) {
+                if (input[j] > input[j+1]) {
+                    int backupVal = input[j+1];
+                    input[j+1] = input[j];
+                    input[j] = backupVal;
+                } else
+                    break;
             }
-            input[j+1] = tmp;
         }
         return input;
     }
