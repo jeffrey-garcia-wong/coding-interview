dca99afa4a950e6ef062b8e93e831bac837ca0c1
jeffrey garcia <jeffrey-garcia@outlook.com>
2023-01-17 14:07:20 +0000

    add initial and complete module for exercise

---
+++ initial/src/test/java/io/jeffrey/core/DemoTests.java
@@ -0,0 +1,13 @@
+package io.jeffrey.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DemoTests {
+
+    @Test
+    public void test() {
+        fail("force fail");
+    }
+}
